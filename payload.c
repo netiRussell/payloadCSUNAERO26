e7ee5355e729cda436332181d0e326a7df100cd3
ruslan <forworkneti@gmail.com>
2025-08-07 10:32:03 -0700

    Attempt to control the onboard RGB

--- payload.c
+++ payload.c
@@ -9,10 +9,13 @@
 #include "driver/gpio.h"
 #include "driver/rmt_rx.h"
 #include "freertos/semphr.h"
+#include "hal/ledc_types.h"
+#include "driver/ledc.h"
 
 // Macros
 #define TRIG_pin_num 16
 #define ECHO_pin_num 18
+#define INTERNAL_RGB_LED 38
 
 // Global structs and variables
 typedef struct {
@@ -48,14 +51,43 @@ void app_main(void)
     // Define a task solely dedicated for terminal print outs
     char* printerTask = pcTaskGetName(NULL);
 
+    // - Pins -
     // Reset the pins
     ESP_ERROR_CHECK( gpio_reset_pin(TRIG_pin_num) );
     ESP_ERROR_CHECK( gpio_reset_pin(ECHO_pin_num) );
+    ESP_ERROR_CHECK( gpio_reset_pin(INTERNAL_RGB_LED) );
+
 
     // Set direction for TRIG and ECHO pins
     ESP_ERROR_CHECK( gpio_set_direction(TRIG_pin_num, GPIO_MODE_OUTPUT) );
     ESP_ERROR_CHECK( gpio_set_direction(ECHO_pin_num, GPIO_MODE_INPUT) );
+    ESP_ERROR_CHECK( gpio_set_direction(INTERNAL_RGB_LED, GPIO_MODE_OUTPUT) );
 
+    // - PWM for RGB LED -
+    // Configure a timer
+    const ledc_timer_config_t rgb_timer_config = {
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .duty_resolution = LEDC_TIMER_8_BIT,
+        .timer_num = 0,
+        .freq_hz = 5000,
+        .clk_cfg = LEDC_USE_XTAL_CLK,
+    };
+    ESP_ERROR_CHECK( ledc_timer_config(&rgb_timer_config) );
+
+    // Configure a channel
+    const ledc_channel_config_t rgb_channel_config = {
+        .gpio_num = INTERNAL_RGB_LED,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .channel = 0,
+        .intr_type = LEDC_INTR_DISABLE,
+        .timer_sel = 0,
+        .duty = 128,
+        .hpoint = 255,
+        .sleep_mode = LEDC_SLEEP_MODE_NO_ALIVE_NO_PD,
+    };
+    ESP_ERROR_CHECK( ledc_channel_config(&rgb_channel_config) );
+
+    // - RMT -
     // Define RMT for ECHO
     rmt_channel_handle_t rx_chan;
 
@@ -115,7 +147,12 @@ void app_main(void)
         if( xSemaphoreTake( RMT_semphr, portMAX_DELAY ) == pdTRUE ){
             ESP_LOGI( printerTask, "Distance = %d", user_ctx_RMT.distance);
             // TODO: Conduct the check once in some time to avoid overloading the core
-            // TODO: Turn on the on-board RGB LED when there is a drone above the sensor
+
+
+            // TO BE DELETED AFTER DEBUGGING: Turn on the on-board RGB LED when there is a drone above the sensor
+            if( user_ctx_RMT.distance <= 7 ){
+                ESP_LOGI( printerTask, "RGB ON");
+            }
         }
     }
 }
